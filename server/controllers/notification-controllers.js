890403dba5661b4a6a6e91aa01528009df3b2153
Neha <lilcuteneha@gmail.com>
2016-01-18 00:38:33 +0530

    Added gulp and ESLint

--- server/controllers/notification-controllers.js
+++ server/controllers/notification-controllers.js
@@ -1,29 +1,31 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Notifications = mongoose.model('Notifications');
+	Notifications = mongoose.model('Notifications');
 
 function getNotificationsCount() {
-    return Notifications.count({read: false}).exec();
+	return Notifications.count({'read': false}).exec();
 }
 
 function getNotifications(date) {
-    if (date) {
-        return Notifications.find({"createdTimestamp": {$lt: date}})
-            .sort('-createdTimestamp')
-            .limit(10)
-            .populate('user')
-            .exec();
-    } else {
-        return Notifications.find({read: false})
-            .sort('-createdTimestamp')
-            .limit(10)
-            .populate('user')
-            .exec();
-    }
+	var promise;
+	if (date) {
+		promise = Notifications.find({'createdTimestamp': {'$lt': date}})
+			.sort('-createdTimestamp')
+			.limit(10)
+			.populate('user')
+			.exec();
+	} else {
+		promise = Notifications.find({'read': false})
+			.sort('-createdTimestamp')
+			.limit(10)
+			.populate('user')
+			.exec();
+	}
+	return promise;
 }
 
 module.exports = {
-    "getNotificationsCount": getNotificationsCount,
-    "getNotifications": getNotifications
+	'getNotificationsCount': getNotificationsCount,
+	'getNotifications': getNotifications
 };
--- server/router/notification-router.js
+++ server/router/notification-router.js
@@ -3,38 +3,36 @@
 var unreadNotifications = [];
 
 var mongoose = require('mongoose'),
-    Notifications = mongoose.model('Notifications');
+	Notifications = mongoose.model('Notifications');
 
 var notificationController = require('./../controllers/notification-controllers');
 
 module.exports = function(app) {
-    app.get('/notifications/count', function (req, res) {
-        notificationController.getNotificationsCount().then(function (count) {
-            res.send({count: count});
-        });
-    });
+	app.get('/notifications/count', function(req, res) {
+		notificationController.getNotificationsCount().then(function(count) {
+			res.send({'count': count});
+		});
+	});
 
-    app.get('/notifications', function (req, res) {
-        console.info(req.data);
-        console.info(req.query);
-        var date = req.query.date;
-        notificationController.getNotifications(date).then(function (notifs) {
-            unreadNotifications = notifs;
-            res.send(notifs);
-        });
-    });
+	app.get('/notifications', function(req, res) {
+		var date = req.query.date;
+		notificationController.getNotifications(date).then(function(notifs) {
+			unreadNotifications = notifs;
+			res.send(notifs);
+		});
+	});
 
-    app.put('/notifications/mark/read', function (req, res) {
-        var notifIdList = unreadNotifications.map(function (notif) {
-            return notif._id;
-        });
-        Notifications.update(
-            {_id: {$in: notifIdList}},
-            {$set: {read: true, updatedTimestamp: Date.now()}},
-            {multi: true}
-        ).exec().then(function (result) {
-            res.send(result)
-        });
-    });
+	app.put('/notifications/mark/read', function(req, res) {
+		var notifIdList = unreadNotifications.map(function(notif) {
+			return notif._id;
+		});
+		Notifications.update(
+			{'_id': {'$in': notifIdList}},
+			{'$set': {'read': true, 'updatedTimestamp': Date.now()}},
+			{'multi': true}
+		).exec().then(function(result) {
+			res.send(result);
+		});
+	});
 
 };

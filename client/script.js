29e6f59cc47a8eefbb51da3d3c0e4b58e24b26a4
Hemkaran Raghav <hemkaran.raghav@wingify.com>
2016-01-18 01:10:28 +0530

    Refactored code and socket io code corrected

--- client/script.js
+++ client/script.js
@@ -1,165 +1,165 @@
 var notification = {
-    	getCount: function() {
-        	$.ajax({
-            	url: '/notifications/count',
-            	success: function(count) {
+    getCount: function () {
+        $.ajax({
+            url: '/notifications/count',
+            success: function (res) {
                 //update the notification count
-                	notification.update(count);
+                notification.update(res.count);
             },
-            	error: function(err) {
-                	console.info('error : ', err);
+            error: function (err) {
+                console.info('error : ', err);
             }
         });
     },
-    	get: function(dateTimestamp) {
-        	$('.loader-container').removeClass('hide');
-        	$.ajax({url: '/notifications',
-            data:{
-                	date: dateTimestamp?dateTimestamp:undefined
+    get: function (dateTimestamp) {
+        $('.loader-container').removeClass('hide');
+        $.ajax({
+            url: '/notifications',
+            data: {
+                date: dateTimestamp ? dateTimestamp : undefined
             },
-            success: function(notifs) {
-                	notification.markAsRead(notifs);
+            success: function (notifs) {
+                notification.markAsRead(notifs);
                 //update the notification count
-                	notification.renderList(notifs);
+                notification.renderList(notifs);
             },
-            error: function(err) {
-                	console.info('error : ', err);
+            error: function (err) {
+                console.info('error : ', err);
             },
-            complete: function() {
-                	$('.loader-container').addClass('hide');
+            complete: function () {
+                $('.loader-container').addClass('hide');
             }
         });
     },
-    	markAsRead: function(notifs) {
-        	$.ajax({
-            	url: '/notifications/mark/read',
-            	type: 'PUT',
-            	success: function(count) {
+    markAsRead: function (notifs) {
+        $.ajax({
+            url: '/notifications/mark/read',
+            type: 'PUT',
+            success: function (res) {
                 //update the notification count
-                	notification.update(count);
+                notification.update(res.count);
             },
-            	error: function(err) {
-                	console.info('error : ', err);
+            error: function (err) {
+                console.info('error : ', err);
             }
         });
     },
-    	update: function(notifs) {
-        	if (notifs.count > 0 && !$('.notif-container').hasClass('open')) {
-            	$('.notification .main-count').removeClass('hide-element');
-            	var node = document.getElementsByClassName('main-count');
-            	console.info(node);
-            	if (notifs.count < 100) {
-                	for (var i = 0; i < node.length; i++) {
-                    	node[i].innerHTML = notifs.count;
+    update: function (count) {
+        if (count > 0 && !$('.notif-container').hasClass('open')) {
+            $('.notification .main-count').removeClass('hide-element');
+            var node = document.getElementsByClassName('main-count');
+            console.info(node);
+            if (count < 100) {
+                for (var i = 0; i < node.length; i++) {
+                    node[i].innerHTML = count;
                 }
-
-            } else if (notifs.count > 0) {
-                	for (var i = 0; i < node.length; i++) {
-                    	node[i].innerHTML = '99+';
-                    	node[i].style.fontSize = '9px';
-                    	node[i].style.lineHeight = '20px';
+            } else {
+                for (var i = 0; i < node.length; i++) {
+                    node[i].innerHTML = '99+';
+                    node[i].style.fontSize = '9px';
+                    node[i].style.lineHeight = '20px';
                 }
             }
         }
+
+        if(count <= 0) {
+            $('.notification .main-count').addClass('hide-element');
+        }
     },
-    	init: function() {
-        	var socket;
+    init: function () {
+        var socket;
 
-        	notification.getCount();
-
-        	socket = io.connect();
-        	socket.on('notificationCount', function(count) {
+        notification.getCount();
+        socket = io.connect();
+        socket.on('notificationCount', function (res) {
             //update the notification count
-            	notification.update(count);
-            	if ($('.notif-container').hasClass('open')) {
-                	notification.get();
+            notification.update(res.count);
+            if ($('.notif-container').hasClass('open')) {
+                notification.get();
             }
         });
     },
-    	togglePanel: function(e) {
-        	if (!$('.notif-container').hasClass('open')) {
-            	notification.get();
+    togglePanel: function () {
+        if (!$('.notif-container').hasClass('open')) {
+            notification.get();
         } else {
-            	$('.notification .main-count').removeClass('hide-element');
+            $('.notification .main-count').removeClass('hide-element');
         }
-        //$('.list-item').addClass('read');
-        	$('.notif-container').toggleClass('open');
-        //$('.notification .main-count').toggleClass('hide-element');
+        $('.notif-container').toggleClass('open');
     },
-    	renderList: function(notifs) {
-        	$('.notification .main-count').addClass('hide-element');
-        	var notifDOM = document.getElementsByClassName('list')[0];
-        	if (notifs.length) {
-            	$('.notif-container .main-count').removeClass('hide-element');
+    renderList: function (notifs) {
+        $('.notification .main-count').addClass('hide-element');
+        var notifDOM = document.getElementsByClassName('list')[0];
+        if (notifs.length) {
+            $('.notif-container .main-count').removeClass('hide-element');
             //notification.removeOldList(notifDOM, notifs.length);
-            	$('.list-item').addClass('read');
+            $('.list-item').addClass('read');
 
-            	notifs.forEach(function(notif) {
-                	var notifTime = notification.formatDatetime(notif.createdTimestamp);
-                	var listNode = document.createElement('div');
-                	listNode.setAttribute('data-date', notif.createdTimestamp);
-                	listNode.className = 'list-item pa-l';
-                	var userImg = document.createElement('img');
-                	userImg.className = 'pull-left';
-                	userImg.src = 'static/img/'+ notif.user.imgName;
-                	var contentDiv = document.createElement('div');
-                	contentDiv.className = 'content';
-                	contentDiv.innerHTML = '<span>'+ notif.user.firstName + ' </span>' + notif.desc;
-                	var timeDiv = document.createElement('div');
-                	timeDiv.className = 'notif-time';
-                	timeDiv.innerText = notifTime;
-                	listNode.appendChild(userImg);
-                	listNode.appendChild(contentDiv);
-                	listNode.appendChild(timeDiv);
+            notifs.forEach(function (notif) {
+                var notifTime = notification.formatDatetime(notif.createdTimestamp);
+                var listNode = document.createElement('div');
+                listNode.setAttribute('data-date', notif.createdTimestamp);
+                listNode.className = 'list-item pa-l';
+                var userImg = document.createElement('img');
+                userImg.className = 'pull-left';
+                userImg.src = 'static/img/' + notif.user.imgName;
+                var contentDiv = document.createElement('div');
+                contentDiv.className = 'content';
+                contentDiv.innerHTML = '<span>' + notif.user.firstName + ' </span>' + notif.desc;
+                var timeDiv = document.createElement('div');
+                timeDiv.className = 'notif-time';
+                timeDiv.innerText = notifTime;
+                listNode.appendChild(userImg);
+                listNode.appendChild(contentDiv);
+                listNode.appendChild(timeDiv);
 
-                	notifDOM.insertBefore(listNode, notifDOM.childNodes[1]);
+                notifDOM.insertBefore(listNode, notifDOM.childNodes[1]);
             });
         } else {
-            	$('.list-item').addClass('read');
-            	$('.notif-container .main-count').addClass('hide-element');
+            $('.list-item').addClass('read');
+            $('.notif-container .main-count').addClass('hide-element');
         }
     },
-    	removeOldList: function(notifDOM, newNotifsLen) {
-        	if (notifDOM.children.length + newNotifsLen > 10) {
-            	if (newNotifsLen > 10) {
-                	$(notifDOM).find('.list-item').slice(0).remove();
+    removeOldList: function (notifDOM, newNotifsLen) {
+        if (notifDOM.children.length + newNotifsLen > 10) {
+            if (newNotifsLen > 10) {
+                $(notifDOM).find('.list-item').slice(0).remove();
             } else {
-                	$(notifDOM).find('.list-item').slice(10-newNotifsLen).remove();
+                $(notifDOM).find('.list-item').slice(10 - newNotifsLen).remove();
             }
         }
     },
-    	formatDatetime : function(dateTime) {
-        	var date= new Date(dateTime);
-        	var year = date.getFullYear(),
-            	month = date.getMonth() + 1, // months are zero indexed
-            	day = date.getDate(),
-            	hour = date.getHours(),
-            	minute = date.getMinutes(),
-            	second = date.getSeconds(),
-            	hourFormatted = hour % 12 || 12, // hour returned in 24 hour format
-            	minuteFormatted = minute < 10 ? '0' + minute : minute,
-            	morning = hour < 12 ? 'am' : 'pm';
+    formatDatetime: function (dateTime) {
+        var date = new Date(dateTime);
+        var year = date.getFullYear(),
+            month = date.getMonth() + 1, // months are zero indexed
+            day = date.getDate(),
+            hour = date.getHours(),
+            minute = date.getMinutes(),
+            second = date.getSeconds(),
+            hourFormatted = hour % 12 || 12, // hour returned in 24 hour format
+            minuteFormatted = minute < 10 ? '0' + minute : minute,
+            morning = hour < 12 ? 'am' : 'pm';
 
-        	return month + '/' + day + '/' + year + ' ' + hourFormatted + ':' +
+        return month + '/' + day + '/' + year + ' ' + hourFormatted + ':' +
             minuteFormatted + morning;
     }
 };
 
-$(document).ready(function() {
-    	notification.init();
+$(document).ready(function () {
+    notification.init();
 
-    	$(document).click(function() {
-        	$('.notif-container').removeClass('open');
+    $(document).click(function () {
+        $('.notif-container').removeClass('open');
     });
 
-    	$('.notification, .notif-container').click(function(e) {
-        	e.stopPropagation();
+    $('.notification, .notif-container').click(function (e) {
+        e.stopPropagation();
     });
 
-    	$('.list').scroll( function() {
-        	console.log($(this)[0].scrollHeight - $(this).scrollTop() + ' == ' + $(this).innerHeight());
-        	if ($(this)[0].scrollHeight - $(this).scrollTop() === $(this).innerHeight()) {
-            	notification.get($(this).find('.list-item').last().attr('data-date'));
+    $('.list').scroll(function () {
+        if ($(this)[0].scrollHeight - $(this).scrollTop() === $(this).innerHeight()) {
+            notification.get($(this).find('.list-item').last().attr('data-date'));
         }
     });
 });
--- server.js
+++ server.js
@@ -1,56 +1,56 @@
 var express = require('express'),
-	path = require('path'),
-	app = express(),
-	http = require('http'),
-	io = require('socket.io'),
-	mongoose = require('mongoose');
+    path = require('path'),
+    app = express(),
+    http = require('http'),
+    io = require('socket.io'),
+    mongoose = require('mongoose');
 
 require('./server/models/notification.server.model');
 require('./server/models/user.server.model');
 
 var Notifications = mongoose.model('Notifications'),
-	notificationCollection = require('./server/data/notifications.json');
+    notificationCollection = require('./server/data/notifications.json');
 
 var notificationController = require('./server/controllers/notification-controllers');
 
 var DB_HOST = process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost',
-	dbConfig = {
-		'uri': 'mongodb://' + DB_HOST + '/fullstack-notification'
-	};
+    dbConfig = {
+        'uri': 'mongodb://' + DB_HOST + '/fullstack-notification'
+    };
 
 app.set('views', './client');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/client'));
 
-io = io.listen(app.listen(3000, function() {
-	console.info('Application started on port : 3000');
+io = io.listen(app.listen(3000, function () {
+    console.info('Application started on port : 3000');
 }));
 
 var db = mongoose.connect(dbConfig.uri);
 db.connection.on('open', function callback() {
-	io.sockets.on('connection', function(socket) {
+    function getUnreadNotification() {
+        notificationController.getNotificationsCount().then(function (count) {
+            io.emit('notificationCount', {'count': count});
+        });
+    }
 
-		function getUnreadNotification() {
-			notificationController.getNotificationsCount().then(function(count) {
-				socket.emit('notificationCount', {'count': count});
-			});
-		}
+    function insertNotifications() {
+        var i = parseInt((Math.random() * notificationCollection.length), 10);
+        Notifications.create(notificationCollection[i], function (err, docs) {
+            if (err) {
+                console.error('Unable to save data : ', err);
+            } else {
+                getUnreadNotification();
+            }
+        });
+        setTimeout(insertNotifications, 10000);
+    }
 
-		function insertNotifications() {
-			var i = parseInt((Math.random() * notificationCollection.length), 10);
-			Notifications.create(notificationCollection[i], function(err, docs) {
-				if (err) {
-					console.error('Unable to save data : ', err);
-				} else {
-					getUnreadNotification();
-				}
-			});
-		}
+    insertNotifications();
 
-		insertNotifications();
-		setInterval(insertNotifications, 10000);
-
-	});
+    io.sockets.on('connection', function () {
+        console.log('A user is connected');
+    });
 });
 
 require('./server/router/common-router')(app);
